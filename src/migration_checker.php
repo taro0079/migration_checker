0d0086721801ac8788628be2fffa991dc02d0520
Morita Taro <38848223+taro0079@users.noreply.github.com>
2024-07-16 00:43:41 +0900

    save

--- src/migration_checker.php
+++ src/migration_checker.php
@@ -1,5 +1,10 @@
 <?php
 
+use MigrationChecker\DbConnector;
+use MigrationChecker\EntityParser;
+use MigrationChecker\FileReader;
+use MigrationChecker\Verification;
+
 function getFilePathsFrom(array $argv): array
 {
     $argv       = array_slice($argv, 1);
@@ -20,8 +25,17 @@ function main(): void
     global $argv;
     try {
         $file_paths = getFilePathsFrom($argv);
+        $mysqli     = new mysqli('127.0.0.1', 'root', '!ChangeMe!', 'app_db');
+        foreach ($file_paths as $file_path) {
+            $file_reader        = new FileReader($file_path);
+            $entity_parser      = new EntityParser($file_reader);
+            $database_connector = new DbConnector($mysqli);
+            $verify             = new Verification($database_connector, $entity_parser);
+            $errorMessages      = $verify->columnCheck();
+        }
     } catch(Exception $e) {
         fwrite(STDERR, $e->getMessage());
+        exit(1);
     }
 }
 
